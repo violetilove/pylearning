3a4e8e4f77c0e07f68d5d4ef3e0cff9c113756a1
violetilove <2786084506@xiaomi.com>
2024-01-28 23:57:27 +0800

    The 8th commit

--- dictionary_note.py
+++ dictionary_note.py
@@ -21,3 +21,25 @@ favorite_language = favorite_languages.get('shy', 'No existing')
 print(favorite_language)
 favorite_language = favorite_languages.get('shy')   # 调用get()时，如果没有指定第二个参数且指定的键不存在，Python将返回值None，
 print(favorite_language)                            # 这个特殊值表示没有相应的值，而是一个表示所需值不存在的特殊值
+print("\n")
+
+for name, language in favorite_languages.items():   # items()返回一个键值对列表
+    print(name, language)
+print(favorite_languages.items())           # item()返回的键值对列表其中的元素为键与值组成的元组
+favorite_languages_list = [('jen', 'python'), ('sarah', 'c'), ('phil', 'ruby'), ]
+for name, language in favorite_languages_list:
+    print(name, language)
+print("\n")
+for name in favorite_languages.keys():              # keys()返回键列表
+    print(name)
+print("\n")
+for name in favorite_languages:       # 遍历字典时，会默认遍历所有的键，因此不使用key()也可以读取键
+    print(name)
+print("\n")
+for language in favorite_languages.values():        # values()返回值列表
+    print(language)
+print("\n")
+favorite_languages['edward'] = 'python'
+for language in set(favorite_languages.values()):        # set()可以找出列表中独一无二的元素，并使用这些元素创建一个集合
+    print(language)
+print(set(favorite_languages.values()))
